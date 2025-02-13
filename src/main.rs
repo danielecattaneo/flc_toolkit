9989032a13548235ee774f6e1708002070f15a9a
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2025-02-13 04:21:15 +0100

    Implement frontend commands for berry-sethi.

--- src/main.rs
+++ src/main.rs
@@ -78,7 +78,39 @@ fn cmd_echo_regex(args: &[String]) -> Option<&[String]> {
     let re_str = &args[0];
     let mut pars = RegexParser::new(re_str);
     if let Some(re) = pars.parse_regex() {
-        println!("{}", re.to_string());
+        println!("{}", re);
+    }
+    return Some(&args[1..]);
+}
+
+fn cmd_berry_sethi(args: &[String]) -> Option<&[String]> {
+    if args.len() < 1 {
+        eprintln!("error: missing argument to \"echo_regex\" command");
+        return None;
+    }
+    let re_str = &args[0];
+    let mut pars = RegexParser::new(re_str);
+    if let Some(re) = pars.parse_regex() {
+        eprintln!("{}", re.to_string_numbered());
+        
+        let mut ini: Vec<_> = re.numbered_initials().into_iter().map(|(c, i)| { format!("{}{}", c, i) }).collect();
+        ini.sort();
+        if re.nullable() { ini.push("⊣".to_string()); }
+        eprintln!("Ini = {{{}}}", ini.join(", "));
+
+        let fin = re.numbered_finals();
+
+        let mut fin_tmp: Vec<_> = re.numbered_followers().iter().map(|(&(c, i), fol)| {
+            let mut str_fol: Vec<_> = fol.into_iter().map(|(c, i)| { format!("{}{}", c, i) }).collect();
+            str_fol.sort();
+            if fin.contains(&(c, i)) { str_fol.push("⊣".to_string()); }
+            (i, format!("Fol({}{}) = {{{}}}", c, i, str_fol.join(", ")))
+        }).collect();
+        fin_tmp.sort();
+        let fin: Vec<_> = fin_tmp.iter().map(|(_, s)| s.clone()).collect();
+        eprintln!("{}", fin.join("\n"));
+        
+        println!("{}", berry_sethi(&re).to_dot(false));
     }
     return Some(&args[1..]);
 }
@@ -100,6 +132,8 @@ fn main() -> ExitCode {
             cmd_echo_mnet(&args_left[1..])
         } else if cmd == "echo_regex" {
             cmd_echo_regex(&args_left[1..])
+        } else if cmd == "berry_sethi" {
+            cmd_berry_sethi(&args_left[1..])
         } else if cmd == "help" || cmd == "-h" || cmd == "--help" {
             eprintln!("help requested");
             help();
